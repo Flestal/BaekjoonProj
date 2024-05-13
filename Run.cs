67f6d99d43a315e19b364ad70da77d6d30c7df5d
flestal <y2s0605@gmail.com>
2024-05-13 17:14:01 +0900

    20240513 commit

--- Run.cs
+++ Run.cs
@@ -10,7 +10,7 @@ namespace BaekjoonProj
     {
         static void Main(string[] args)
         {
-            _5217.Run(args);
+            _2304.Run(args);
         }
     }
 }
--- obj/Debug/net7.0/BaekjoonProj.AssemblyInfo.cs
+++ obj/Debug/net7.0/BaekjoonProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaekjoonProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f86749b7a87a6cabbfb34bf7f036e4f1f59745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6fcdb92d24ce3e1b21eef7670232fa0751c118")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaekjoonProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaekjoonProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
