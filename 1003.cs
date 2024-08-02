140cd8ae140ab4e8eb75902efc92aa05eb6b84ed
flestal <y2s0605@gmail.com>
2024-08-02 16:37:11 +0900

    20240802 commit

---
+++ 1003.cs
@@ -0,0 +1,28 @@
+﻿namespace BaekjoonProj
+{
+    internal class _1003
+    {
+        public static void Run(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            (int, int)[] fibo = new (int, int)[41];
+            fibo[0] = (1, 0);
+            fibo[1] = (0, 1);
+            for(int i = 2;i<41;i++)
+            {
+                fibo[i] =
+                    (
+                    fibo[i - 1].Item1 + fibo[i - 2].Item1,
+                    fibo[i - 1].Item2 + fibo[i - 2].Item2
+                    );
+            }
+            for(int i=0;i<T;i++)
+            {
+                int N = int.Parse(Console.ReadLine());
+                sb.AppendLine(fibo[N].Item1 +" " + fibo[N].Item2);
+            }
+            Console.WriteLine( sb );
+        }
+    }
+}
